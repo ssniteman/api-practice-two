46abf2550189e14184339a8496f89f793137e683
Shane Sniteman <shanesniteman@Shanes-MacBook-Pro.local>
2014-02-26 23:16:28 -0500

    working on view

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,7 +1,22 @@
 var MainRouter = Backbone.Router.extend({
 
+	routes: {
+		"poler/:videos" : "polerVideo"
+	},
 
+	initialize: function(){
 
+		this.videos = new VideosCollection;
+		this.videos.on ('add', function(video){
+			new VideoView({model: video})
+		})
+
+	},
+
+	polerVideo: function(){
+		this.videos.url = "http://vimeo.com/api/v2/polerstuff/videos.json?callback=myFunction"
+	}
+	this.videos.fetch();
 
 
 
